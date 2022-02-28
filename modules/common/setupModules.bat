d81e99e3d6fa05fef64645f3ed36cbbc5e403c2f
KommandNyang <baejungmin077@gmail.com>
2022-02-28 12:52:52 +0900

    Update setupModules.bat

--- modules/common/setupModules.bat
+++ modules/common/setupModules.bat
@@ -4,19 +4,15 @@ cd %PROJECT_DIR%
 echo Please Don't Close This Window
 
 npm init --yes
-
-npm i dotenv 
-
+npm i dotenv
 IF %DJSVER%==12 (
     npm i discord.js@12.5.3
 ) ELSE (
     npm i discord.js@latest
 )
-
 IF %USEKOMM%==1 (
-    npm i discord-kommando.js
+    npm i discord-kommando.js@latest
 )
-
 IF %USEDOK% (
     IF %DJSVER%==12 (
         npm i dokdo@djsv12
@@ -24,11 +20,9 @@ IF %USEDOK% (
         npm i dokdo@latest
     )
 )
-
 IF %USEDISBUT%==1 (
-    npm i discord-buttons
+    npm i discord-buttons@4.0.0
 )
-
 echo Finished installing modules
 echo You can close this Window now
 echo ----------------------------------------------------
